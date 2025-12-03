19186483f6e7044971c3720adaeea22ca3ecb6f9
Michael Eco <michael.gunawan2002@gmail.com>
2025-12-03 12:15:25 +0700

    feat: added graph state

---
+++ core/graph/state.py
@@ -0,0 +1,20 @@
+from typing import List, TypedDict, Optional, Union, Annotated
+from langchain_core.messages import AnyMessage
+from langgraph.graph.message import add_messages
+
+class BookingDetails(TypedDict):
+    name: Optional[str]
+    phone_number: Optional[str]
+    service_type: Optional[str]
+    doctor: Optional[str]
+    date: Optional[str]
+    time: Optional[str]
+    booking_id: Optional[str]
+
+class GraphState(TypedDict):
+    messages: Annotated[List[AnyMessage], add_messages]
+    query: str
+    documents: List[str]
+    booking_details: BookingDetails
+    next_step: str
+    tool_status: str
