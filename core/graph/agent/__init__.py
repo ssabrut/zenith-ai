e54753a11839d5592d360918865d76f7e5211204
Michael Eco <michael.gunawan2002@gmail.com>
2025-12-09 14:01:10 +0700

    feat: added SQL node

--- core/graph/agent/__init__.py
+++ core/graph/agent/__init__.py
@@ -1,4 +1,5 @@
 from .general import GeneralAgent
 from .inquiry import InquiryAgent
+from .sql import SQLAgent
 
-__all__ = ["GeneralAgent", "InquiryAgent"]
+__all__ = ["GeneralAgent", "InquiryAgent", "SQLAgent"]
